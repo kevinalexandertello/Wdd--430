6ca4a131cdbde0ea6948881e76d0d025043d0120
kevinalexandertello <gaston_alexander@hotmail.es>
2021-09-26 02:36:32 -0300

    yes

--- cms/src/app/contacts/contacts.component.ts
+++ cms/src/app/contacts/contacts.component.ts
@@ -9,8 +9,8 @@ import { Contact } from './contact.model';
 })
 export class ContactComponent implements OnInit {
   contacts: Contact[] = [
-    new Contact('1', 'R.Kent Jackson', 'jacksonk@byui.edu' , '208-496-3771' , './images/jacksonk.jpg' , 'null'),
-    new Contact('2' , 'Rex Barzee', 'barzeer@byui.edu' , '208-496-3768' , './images/barzeer.jpg' , 'null')
+    new Contact('1', 'R.Kent Jackson', 'jacksonk@byui.edu' , '208-496-3771' , '.../images/jacksonk.jpg' , 'null'),
+    new Contact('2' , 'Rex Barzee', 'barzeer@byui.edu' , '208-496-3768' , '.../images/barzeer.jpg' , 'null')
   ];
 
   constructor() { }
